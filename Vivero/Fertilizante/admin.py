418d70998003388a6c8a43c20de618bbd343d9c7
Richard <ricfcuesta123@gmail.com>
2023-05-22 21:44:49 -0500

    Modificar campo control en base de datos

--- Vivero/Fertilizante/admin.py
+++ Vivero/Fertilizante/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import fertilizante
 # Register your models here.
+admin.site.register(fertilizante)
--- Vivero/Viveros/admin.py
+++ Vivero/Viveros/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import vivero, ubicacion
 # Register your models here.
+admin.site.register(vivero)
+admin.site.register(ubicacion)
